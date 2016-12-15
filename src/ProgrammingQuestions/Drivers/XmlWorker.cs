e34606eed2c1a6358c85e704fbc5fcfee1e13215
Joshua Niederer <josh@jpniederer.com>
2016-12-14 22:48:18 -0600

    Forgot to update the path of the xml file.

--- src/ProgrammingQuestions/Drivers/XmlWorker.cs
+++ src/ProgrammingQuestions/Drivers/XmlWorker.cs
@@ -12,7 +12,7 @@ namespace ProgrammingQuestions.Drivers
         public static ProgrammingQuestionCollection GetQuestions(Microsoft.AspNetCore.Hosting.IHostingEnvironment _env)
         {
             XmlSerializer ser = new XmlSerializer(typeof(ProgrammingQuestionCollection));
-            FileStream myFileStream = new FileStream(_env.ContentRootPath + "\\Data\\ProgrammingQuestions.xml", FileMode.Open);
+            FileStream myFileStream = new FileStream(_env.ContentRootPath + "\\QuestionBank\\ProgrammingQuestions.xml", FileMode.Open);
             ProgrammingQuestionCollection pqc = ((ProgrammingQuestionCollection)ser.Deserialize(myFileStream));
             myFileStream.Dispose();
 
